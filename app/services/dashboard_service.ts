7a3f86203d0de75bc6d3b3dd515463a662609f23
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2024-09-18 19:48:25 -0300

    Dashboard: byCropPlanted and byArea

--- app/services/dashboard_service.ts
+++ app/services/dashboard_service.ts
@@ -1,24 +1,29 @@
 import RuralProducer from "#models/rural_producer";
+import CropsPlantedService from "#services/crops_planted_service";
 
 export default class DashboardService {
-  async data() {
-    const allRuralProducers = await RuralProducer.all()
+  private ruralProducers: RuralProducer[] = [];
 
-    const farmsArea = allRuralProducers.reduce((acc, obj) => acc + obj.total_area, 0)
+  async data() {
+    this.ruralProducers = await RuralProducer.all()
+
+    const farmsArea = this.ruralProducers.reduce((acc, obj) => acc + obj.total_area, 0)
 
     return {
-      totalFarms: allRuralProducers.length,
+      totalFarms: this.ruralProducers.length,
       farmsArea,
-      states: await this.byState(allRuralProducers)
+      states: await this.byState(),
+      cropsPlanted: await this.byCropPlanted(),
+      arableArea: await this.byArea(),
     }
   }
 
-  async byState(ruralProducers: RuralProducer[]) {
+  async byState() {
     const totals: { [key: string]: number } = {};
     const percentages: { [key: string]: number } = {};
     let grandTotal = 0;
 
-    for (const ruralProducer of ruralProducers) {
+    for (const ruralProducer of this.ruralProducers) {
       if (!totals[ruralProducer.state]) totals[ruralProducer.state] = 0;
       totals[ruralProducer.state] += 1;
     }
@@ -39,6 +44,51 @@ export default class DashboardService {
         ALL_STATES: grandTotal,
       },
       percentages,
+    }
+  }
+
+  async byCropPlanted() {
+    const totals: { [key: string]: number } = {};
+    const percentages: { [key: string]: number } = {};
+    let grandTotal = 0;
+
+    for (const ruralProducer of this.ruralProducers) {
+      const cropsPlanted = await new CropsPlantedService(ruralProducer.id).getCropsPlanted()
+
+      for (const crop_planted of cropsPlanted) {
+        if (!totals[crop_planted['name']])
+          totals[crop_planted['name']] = 1;
+        else
+          totals[crop_planted['name']] += 1;
+      }
+    }
+
+    for (const key of Object.keys(totals)) grandTotal += totals[key];
+
+    for (const key of Object.keys(totals)) {
+      const percentage = Number(((totals[key] / grandTotal) * 100).toFixed(0));
+
+      percentages[key] = percentage || 0;
+    }
+
+    return {
+      totals,
+      percentages,
+    }
+  }
+
+  async byArea() {
+    let totalArable = 0;
+    let totalVegetation = 0;
+
+    for (const ruralProducer of this.ruralProducers) {
+      totalArable += Number(ruralProducer.arable_area);
+      totalVegetation += Number(ruralProducer.vegetation_area);
+    }
+
+    return {
+      totalArable,
+      totalVegetation,
     }
   }
 }
