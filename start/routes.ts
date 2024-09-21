c5bc9d7d789dc04596a4850394ce4fb157f618d5
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2024-09-21 12:08:59 -0300

    Autoswagger removed

--- start/routes.ts
+++ start/routes.ts
@@ -1,22 +1,8 @@
-import AutoSwagger from 'adonis-autoswagger'
-import swagger from '#config/swagger'
 import router from '@adonisjs/core/services/router'
 
 const DashboardController = () => import('#controllers/dashboard_controller')
 const RuralProducersController = () => import('#controllers/rural_producers_controller')
 const CropsPlantedController = () => import('#controllers/crops_planted_controller')
-
-// returns swagger in YAML
-router.get('/swagger', async () => {
-  return AutoSwagger.default.docs(router.toJSON(), swagger)
-})
-
-// Renders Swagger-UI and passes YAML-output of /swagger
-router.get('/docs', async () => {
-  return AutoSwagger.default.ui('/swagger', swagger)
-  // return AutoSwagger.default.scalar("/swagger"); to use Scalar instead
-  // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
-})
 
 router
   .group(() => {
